2bb41883f9ff5a299c939eb5aaf32acb10ad8f18
rupert <rupert@indigo-tangerine.nl>
2022-07-22 13:38:56 +0200

    add pnl cdk repos

--- terraform/_repos.tf
+++ terraform/_repos.tf
@@ -14,6 +14,22 @@ module "tfm_projects" {
   gh_team_developers_id                = github_team.developers.id
 }
 
+module "cdk_projects" {
+  source = "./modules/gh_repo"
+
+  ecr_repositories                     = local.cdk_projects.ecr_repositories
+  github_repositories                  = local.cdk_projects.github_repositories
+  group_name                           = local.cdk_projects.group_name
+  codeowners                           = local.cdk_projects.codeowners
+  ecr_get_permissions_allowed_accounts = local.cdk_projects.ecr_get_permissions_allowed_accounts
+  ecr_put_permissions_allowed_accounts = local.cdk_projects.ecr_put_permissions_allowed_accounts
+  github_org                           = var.github_org
+  gh_team_admins_id                    = github_team.admins.id
+  gh_team_admins_node_id               = github_team.admins.node_id
+  gh_team_admins_slug                  = github_team.admins.slug
+  gh_team_developers_id                = github_team.developers.id
+}
+
 locals {
   tfm_projects = {
     group_name                           = "tfm-projects"
@@ -57,6 +73,23 @@ locals {
         create_from_template = {
           repository = "itc-tfm-project-template"
         }
+      }
+    }
+  }
+
+  cdk_projects = {
+    group_name                           = "cdk-projects"
+    codeowners                           = []
+    ecr_get_permissions_allowed_accounts = [data.aws_caller_identity.current.account_id]
+    ecr_put_permissions_allowed_accounts = [data.aws_caller_identity.current.account_id]
+    ecr_repositories                     = {}
+    github_repositories = {
+      itc-cdk-pnl-demo = {
+        description = "ITC - PNL CDK Demo"
+      }
+      itc-cdk-project-template = {
+        description = "ITC - CDK Project Template"
+        is_template = true
       }
     }
   }
